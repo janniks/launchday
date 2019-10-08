676906d0f1d50c358f64af83d40bf1cf3457a7af
janniks <jannik.siebert@gmail.com>
2019-10-08 21:05:55 +0200

    Integrate initial api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
+import moment from 'moment-timezone'
+
 import 'App.less'
-import Todos from 'pages/Todos'
+import Today from 'pages/Today'
 
 const App = () => {
   return (
     <div className="App">
+      <h1>Launchday</h1>
+      <p>
+        It's currently{' '}
+        {moment()
+          .tz('America/Los_Angeles')
+          .format('hh:mm A')}{' '}
+        at ProductHunt!
+      </p>
       <Router>
-        <Route exact path="/" component={Todos} />
+        <Route exact path="/" component={Today} />
       </Router>
     </div>
   )
--- src/services/TodoService.js
+++ src/services/TodoService.js
@@ -1,18 +1,68 @@
 import axios from 'axios'
+import moment from 'moment-timezone'
+
+import generateColors from 'distinct-colors'
 
 const BASE_URL = process.env.REACT_APP_BASE_URL
-const TODOS_URL = `${BASE_URL}/todos`
+const TODAY_URL = `${BASE_URL}/24h`
 
-export default class TodoService {
-  static async getTodos({ setTodos, setError }) {
+export default class ApiService {
+  static async getToday({ setDatasets, setError }) {
     axios
-      .get(`${TODOS_URL}?size=1000000`)
+      .get(`${TODAY_URL}`)
       .then(response => {
-        setTodos(response.data)
+        console.log('data', response.data)
+        const datasets = transformToDatasets(response.data)
+        setDatasets(datasets)
       })
       .catch(e => {
         console.error(e)
-        setError('getTodos error')
+        setError('getToday error')
       })
+  }
+}
+
+function transformToDatasets(data) {
+  console.log('transform start')
+  const commentDataset = []
+  const voteDataset = []
+
+  const colors = generateColors({
+    count: data.length
+  })
+
+  data.forEach((post, index) => {
+    const commentCoordinates = []
+    const voteCoordinates = []
+    post.counts.forEach(poll => {
+      commentCoordinates.push({
+        x: poll.polled_at,
+        y: poll.comments_count
+      })
+      voteCoordinates.push({
+        x: poll.polled_at,
+        y: poll.votes_count
+      })
+    })
+    commentDataset.push({
+      data: commentCoordinates,
+      label: post.post_name,
+      fill: false,
+      backgroundColor: colors[index],
+      borderColor: colors[index]
+    })
+    voteDataset.push({
+      data: voteCoordinates,
+      label: post.post_name,
+      fill: false,
+      backgroundColor: colors[index],
+      borderColor: colors[index]
+    })
+  })
+
+  console.log('transform complete')
+  return {
+    commentDataset,
+    voteDataset
   }
 }
