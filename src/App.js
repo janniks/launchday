719da9b19eb41b8246dc719faef88404bbc714f8
janniks <jannik.siebert@gmail.com>
2019-10-16 18:17:10 +0200

    Remove legend, add logo, make responsive

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-
-import moment from 'moment-timezone'
-
 import 'App.less'
+
+import { Card, Col, Icon, Layout, Row } from 'antd'
+import moment from 'moment-timezone'
 import Today from 'pages/Today'
+
+import rocket from 'resources/rocket'
+
+const { Header, Footer, Content } = Layout
+
+const RocketIcon = props => <Icon component={rocket} />
+
+const colProps = { xs: { span: 22, offset: 1 }, md: { span: 12, offset: 6 } }
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Launchday</h1>
-      <p>
-        It's currently{' '}
-        {moment()
-          .tz('America/Los_Angeles')
-          .format('hh:mm A')}{' '}
-        at ProductHunt!
-      </p>
-      <Router>
-        <Route exact path="/" component={Today} />
-      </Router>
+    <div>
+      <Layout>
+        <Header>
+          <Row>
+            <Col {...colProps}>
+              <a className="card" href="/">
+                <h1>
+                  Launchday <RocketIcon />
+                </h1>
+              </a>
+            </Col>
+          </Row>
+        </Header>
+        <Content>
+          <Row>
+            <Col {...colProps}>
+              <p>
+                It's currently{' '}
+                {moment()
+                  .tz('America/Los_Angeles')
+                  .format('hh:mm A')}{' '}
+                at ProductHunt!
+              </p>
+              <Router>
+                <Route exact path="/" component={Today} />
+              </Router>
+            </Col>
+          </Row>
+        </Content>
+        <Footer>
+          <Row>
+            <Col {...colProps}>made by @jnnksbrt</Col>
+          </Row>
+        </Footer>
+      </Layout>
     </div>
   )
 }
--- src/pages/Today.js
+++ src/pages/Today.js
@@ -1,12 +1,19 @@
 import React, { useState, useEffect } from 'react'
 
-import { Icon, Result } from 'antd'
-import { Line } from 'react-chartjs-2'
+import { Card, Icon, Result } from 'antd'
+import { defaults, Line } from 'react-chartjs-2'
 import moment from 'moment-timezone'
 
 import DataService from 'services/DataService'
 
 const timezoneOffset = moment().utcOffset() / 60
+
+defaults.global.defaultFontFamily =
+  "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'PingFang SC', 'Hiragino Sans GB', 'Microsoft YaHei', 'Helvetica Neue', Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'"
+
+defaults.global.legend.display = false
+
+const chartHeight = Math.max(400, window.innerHeight / 1.5)
 
 const Today = () => {
   const [error, setError] = useState(false)
@@ -31,64 +38,86 @@ const Today = () => {
 
   return (
     <div>
-      <h2>Today</h2>
-      <Line
-        data={{ datasets: datasets.voteDataset }}
-        height={1000}
-        options={{
-          scales: {
-            xAxes: [
-              {
-                type: 'time',
-                time: {
-                  parser: t =>
-                    moment(t)
+      <Card title="Today's Products">
+        <div>
+          <Line
+            data={{ datasets: datasets.voteDataset }}
+            height={chartHeight}
+            options={{
+              scales: {
+                xAxes: [
+                  {
+                    type: 'time',
+                    time: {
+                      parser: t =>
+                        moment(t)
+                          .tz('America/Los_Angeles')
+                          .utcOffset(timezoneOffset, true)
+                    }
+                  }
+                ],
+                yAxes: [
+                  {
+                    /* keep default axes */
+                  },
+                  {
+                    position: 'right',
+                    ticks: {
+                      display: false
+                    },
+                    gridLines: {
+                      display: false,
+                      drawTicks: false
+                    }
+                  }
+                ]
+              },
+              hover: {
+                mode: 'nearest',
+                intersect: false,
+                animationDuration: 0
+              },
+              elements: {
+                point: {
+                  radius: 0
+                }
+              },
+              animation: {
+                duration: 0
+              },
+              responsiveAnimationDuration: 0,
+              responsive: true,
+              maintainAspectRatio: false,
+              legendCallback: function(chart) {
+                return '<div>this is my legend</div>'
+              },
+              tooltips: {
+                callbacks: {
+                  label: function(tooltipItem, data) {
+                    return ` ${data.datasets[tooltipItem.datasetIndex].label}`
+                  },
+                  labelColor: function(tooltipItem, chart) {
+                    const color =
+                      chart.config.data.datasets[tooltipItem.datasetIndex]
+                        .backgroundColor
+                    return {
+                      borderColor: color,
+                      backgroundColor: color
+                    }
+                  },
+                  title: function(tooltipItems) {
+                    const time = moment(tooltipItems[0].label)
                       .tz('America/Los_Angeles')
-                      .utcOffset(timezoneOffset, true)
+                      .utcOffset(0, true)
+                      .format('hh:mm A')
+                    return `${tooltipItems[0].yLabel} votes - ${time}`
+                  }
                 }
               }
-            ]
-          },
-          hover: {
-            mode: 'nearest',
-            intersect: false,
-            animationDuration: 0
-          },
-          elements: {
-            point: {
-              radius: 0
-            }
-          },
-          animation: {
-            duration: 0
-          },
-          responsiveAnimationDuration: 0,
-          maintainAspectRatio: false,
-          tooltips: {
-            callbacks: {
-              label: function(tooltipItem, data) {
-                return ` ${data.datasets[tooltipItem.datasetIndex].label}`
-              },
-              labelColor: function(tooltipItem, chart) {
-                const color =
-                  chart.config.data.datasets[tooltipItem.datasetIndex]
-                    .backgroundColor
-                return {
-                  borderColor: color,
-                  backgroundColor: color
-                }
-              },
-              title: function(tooltipItems) {
-                const time = moment(tooltipItems[0].label)
-                  .tz('America/Los_Angeles')
-                  .utcOffset(0, true)
-                  .format('hh:mm A')
-                return `${tooltipItems[0].yLabel} votes - ${time}`
-              }
-            }
-          }
-        }}
-      />
+            }}
+          />
+        </div>
+      </Card>
     </div>
   )
 }
